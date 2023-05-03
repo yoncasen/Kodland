3db2709a7cbacc1e4ea8d6df7a02fd04df8d29ca
Yonca <sen.yoncaa@gmail.com>
2023-05-03 18:32:08 +0300

    Week 5 update

---
+++ Python/M2L1/file_read_write/file_rw.py
@@ -0,0 +1,23 @@
+import discord
+
+# Bu kod parçacığı bir metin dosyasının tamamını okumamızı sağlar
+f = open('/Users/yonca/Library/Mobile Documents/com~apple~CloudDocs/Kodland/Python/M2L1/file_read_write/text.txt', 'r', encoding='utf-8')
+text = f.read()
+print(text)
+f.close()
+
+# Ve işte bir metin dosyasının tamamını nasıl yeniden yazabileceğimiz:
+""" f = open('metinbelgesi.txt', 'w', encoding='utf-8')
+text = 'Yeni Yazı'
+f.write(text)
+f.close() """
+
+# Kısa hali:
+""" with open('text.txt', 'r', encoding='utf-8') as f:
+    print(f.read()) """
+
+""" with open('/Users/yonca/Library/Mobile Documents/com~apple~CloudDocs/Kodland/Python/M2L1/file_read_write/cat.jpg', 'rb') as f:
+        print("Image is processing...")
+        picture = discord.File(f) """
+
+        
