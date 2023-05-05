ca2f13a764126b5c02f28bacd570846468db39e4
Yonca <sen.yoncaa@gmail.com>
2023-05-05 22:07:28 +0300

    Week 5 Update

--- Python/M1L4/bot_bot.py
+++ Python/M1L4/bot_bot.py
@@ -1,4 +1,5 @@
 import discord
+import random
 from discord.ext import commands
 from bot_token import token 
 
@@ -19,4 +20,16 @@ async def hello(ctx):
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
 
+# @bot.command()
+# async def joined(ctx, member: discord.Member):
+#     """Says when a member joined."""
+#     await ctx.send(f'{member.name} joined')
+
+# @bot.group()
+# async def cool(ctx):
+#     """Says if a user is cool.
+#     In reality this just checks if a subcommand is being invoked.
+#     """
+#     if ctx.invoked_subcommand is None:
+#         await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
 bot.run(token)
--- Python/M2L1/bot_bot.py
+++ Python/M2L1/bot_bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from bot_token import token 
+import requests, os, random
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -18,5 +19,38 @@ async def hello(ctx):
 @bot.command()
 async def heh(ctx, count_heh = 5):
     await ctx.send("he" * count_heh)
+
+@bot.command()
+async def mem(ctx):
+    with open('Python/M2L1/images/mem1.png', 'rb') as f: 
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+
+######## Random Meme
+# @bot.command()
+# async def memr(ctx):
+#     img_name = random.choice(os.listdir('Python/M2L1/images'))
+#     with open(f'Python/M2L1/images/{img_name}', 'rb') as f:
+#         picture = discord.File(f)
+#     
+#     await ctx.send(file=picture)
+
+
+######### Extra Task API
+# def get_duck_image_url():
+#     url = 'https://random-d.uk/api/random'
+#     res = requests.get(url)
+#     data = res.json()
+#     return data['url']
+
+# @bot.command('duck')
+# async def duck(ctx):
+#     '''duck komutunu çağırdığımızda, program ordek_resmi_urlsi_al fonksiyonunu çağırır'''
+#     image_url = get_duck_image_url()
+#     await ctx.send(image_url)
+
+
+
 
 bot.run(token)
