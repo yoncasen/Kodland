aae4dc138ddb16a6a4859311c08216af9cb11cae
Yonca <sen.yoncaa@gmail.com>
2023-06-02 17:09:43 +0300

    Week 9 updated, week 10 is added.

--- Python/M1L3/bot_logic.py
+++ Python/M1L3/bot_logic.py
@@ -13,7 +13,6 @@ def gen_emoji():
     emoji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
     return random.choice(emoji)
 
-
 def toss():
     coin = random.randint(0, 2)
     if coin == 0:
