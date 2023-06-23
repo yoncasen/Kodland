682968bc17f90edf61224119604cc0ba1839a842
Yonca <sen.yoncaa@gmail.com>
2023-06-23 20:26:19 +0300

    M3L3 update

--- Python/M2L1/bot_bot.py
+++ Python/M2L1/bot_bot.py
@@ -5,12 +5,20 @@ import requests, os, random
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} olarak giriş yaptık')
+
+@bot.event
+async def on_member_join(member):
+        guild = member.guild
+        if guild.system_channel is not None:
+            to_send = f'Welcome {member.mention} to {guild.name}!'
+            await guild.system_channel.send(to_send)
 
 @bot.command()
 async def hello(ctx):
--- Python/M3L1/main.py
+++ Python/M3L1/main.py
@@ -5,19 +5,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return '<h1>Hello, World!</h1>'
+    return '<h1>Hello, fhfgjhgjWorld!</h1>'
 
 
-""" facts_list = ["Elon Musk, sosyal ağların içeriği görüntülemek için mümkün olduğunca fazla zaman harcamamız için bizi platformun içinde tutmak üzere tasarlandığını iddia ediyor.",
+facts_list = ["Elon Musk, sosyal ağların içeriği görüntülemek için mümkün olduğunca fazla zaman harcamamız için bizi platformun içinde tutmak üzere tasarlandığını iddia ediyor.",
             "2018 yılında yapılan bir araştırmaya göre 18-34 yaş arası kişilerin %50'den fazlası akıllı telefonlarına bağımlı olduğunu düşünüyor.",
             "Sosyal ağların olumlu ve olumsuz yanları var ve bu platformları kullanırken her ikisinin de farkında olmalıyız.",
             "Teknoloji bağımlılığı çalışması, modern bilimsel araştırmanın en alakalı alanlarından biridir."]
-"""
 
-"""
-@app.route("/")
+@app.route("/rastgele_gercek")
 def facts():
-    return f'<p>{random.choice(facts_list)}</p>' """
+    return f'<p>{random.choice(facts_list)}</p>'
 
 """@app.route("/")
 def index():
